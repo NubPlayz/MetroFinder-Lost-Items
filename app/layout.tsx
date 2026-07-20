3e2e6d8a2ffb25a37e6e3dbc6101d21d11bd6f0b
Nub <scriptouroboros@gmail.com>
2026-07-20 11:58:22 +0530

    package removed

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <MikuCursorClient />
         <Analytics />
         <SpeedInsights />
       </body>
